a74b98e9cc99312a962cd6c1775ae9c3a4bf82b7
Sabelo Mk <90407948+LaplaceXero@users.noreply.github.com>
2023-02-09 11:20:22 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int testCases = 0;
+	int x; //Chef's cash
+	int y; //chef's bill
+	bool result;
+	
+	cin >>testCases;
+	
+	for(int i = 0; i < testCases; i++)
+	{
+	    cin >>x >>y;
+	    
+	    result = (x >= y) ? true:false;
+	    
+	    if(result)
+	        cout <<"No" <<endl;
+	        
+	    else
+	        cout <<"Yes" <<endl;
+	}
+	return 0;
+}
